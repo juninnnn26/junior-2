06622e7d027fb6e7f7173718661ca34822516b32
juninnnn26 <juniorgivaldo09@gmail.com>
2024-11-21 21:00:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Quantas linhas tem no logo do serviço de streaming de música Spotify?',
+    '3 linhas'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o formato do salgadinho Doritos??',
+    'Triangular.'
+)
+
+criaCartao(
+    'Geral',
+    'Qual animal representa o símbolo da paz?',
+    ' A pomba branca.'
+)
+
+criaCartao(
+    'Geral',
+    'Qual é o animal que aparece no logotipo da Ferrari?',
+    'Cavalo.'
+)
